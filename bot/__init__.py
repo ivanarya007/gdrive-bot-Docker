dc445f186dc09fa5ea4c033b311b43d25cf58b87
ivanarya007 <97018175+ivanarya007@users.noreply.github.com>
2023-04-10 16:38:41 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 import os
 import logging
+
+load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,32 +13,19 @@ LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-ENV = bool(os.environ.get('ENV', False))
-try:
-  if ENV:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    APP_ID = os.environ.get('APP_ID')
-    API_HASH = os.environ.get('API_HASH')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    SUDO_USERS = os.environ.get('SUDO_USERS')
-    SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK')
-    DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads/")
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID")
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET")
-  else:
-    from bot.config import config
-    BOT_TOKEN = config.BOT_TOKEN
-    APP_ID = config.APP_ID
-    API_HASH = config.API_HASH
-    DATABASE_URL = config.DATABASE_URL
-    SUDO_USERS = config.SUDO_USERS
-    SUPPORT_CHAT_LINK = config.SUPPORT_CHAT_LINK
-    DOWNLOAD_DIRECTORY = config.DOWNLOAD_DIRECTORY
-    G_DRIVE_CLIENT_ID = config.G_DRIVE_CLIENT_ID
-    G_DRIVE_CLIENT_SECRET = config.G_DRIVE_CLIENT_SECRET
-  SUDO_USERS = list(set(int(x) for x in SUDO_USERS.split()))
-  SUDO_USERS.append(939425014)
-  SUDO_USERS = list(set(SUDO_USERS))
-except KeyError:
-  LOGGER.error('One or more configuration values are missing exiting now.')
-  exit(1)
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+APP_ID = int(os.getenv('APP_ID'))
+API_HASH = os.getenv('API_HASH')
+DATABASE_URL = os.getenv('DATABASE_URL')
+SUDO_USERS = os.getenv('SUDO_USERS')
+SUPPORT_CHAT_LINK = os.getenv('SUPPORT_CHAT_LINK')
+DOWNLOAD_DIRECTORY = os.getenv("DOWNLOAD_DIRECTORY", "./downloads/")
+G_DRIVE_CLIENT_ID = os.getenv("G_DRIVE_CLIENT_ID")
+G_DRIVE_CLIENT_SECRET = os.getenv("G_DRIVE_CLIENT_SECRET")
+
+
+if SUDO_USERS:
+    SUDO_USERS = list(map(int, SUDO_USERS.split(',')))
+else:
+    SUDO_USERS = [939425014] # Default sudo user is included here
+SUDO_USERS = list(set(SUDO_USERS))
